269d98a3c6ae813b8664aa497b8ed38b6acfbb11
jamesvovos <james.vovos@gmail.com>
2023-04-07 15:02:59 +1000

    added CORS middleware

--- endpoints.py
+++ endpoints.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from database.database import engine, SessionLocal
 from sqlalchemy.orm import Session
 import schemas
@@ -22,6 +23,17 @@ def get_db():
 
 # create FastAPI object
 app = FastAPI()
+
+# Set up CORS middleware
+origins = ["http://localhost",    "http://localhost:3000", ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # API Requests/Endpoints
